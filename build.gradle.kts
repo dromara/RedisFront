f2ca971e2894afc709fc22d08f42c7b0079d9c65
westboy <jin@westboy.net>
2022-07-07 13:09:50 +0800

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 plugins {
     id("java")
@@ -7,110 +8,99 @@ plugins {
 group = "com.redisfront"
 version = "1.0-SNAPSHOT"
 
-var flatlafVersion = "2.3"
-var hutoolVersion = "5.8.3"
-var jedisVersion = "4.2.3"
-var fifesoftVersion = "3.2.0"
-var derbyVersion = "10.15.2.0"
+val flatlafVersion = "2.3"
+val hutoolVersion = "5.8.3"
+val jedisVersion = "4.2.3"
+val fifesoftVersion = "3.2.0"
+val derbyVersion = "10.15.2.0"
+val lettuceVersion = "6.1.8.RELEASE"
+val gsonVersion = "2.9.0"
+val logbackVersion = "1.2.11"
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 
 if (JavaVersion.current() < JavaVersion.VERSION_17)
-    throw RuntimeException("required ( Java ${JavaVersion.VERSION_17})")
+    throw RuntimeException("compile required Java ${JavaVersion.VERSION_17}, current Java ${JavaVersion.current()}")
 
+val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
 
 println()
 println("-------------------------------------------------------------------------------")
-println("RedisFront Version: ${version}")
-println("Gradle Path: ${gradle.gradleVersion} at ${gradle.gradleHomeDir}")
-println("Java Version: ${System.getProperty("java.version")}")
-println("Build Date: ${LocalDateTime.now()}")
+println("$name Version: $version")
+println("Project Path:  $projectDir")
+println("Java Version:  ${System.getProperty("java.version")}")
+println("Gradle Version: ${gradle.gradleVersion} at ${gradle.gradleHomeDir}")
+println("Current Date:  ${LocalDateTime.now().format(dateTimeFormatter)}")
 println("-------------------------------------------------------------------------------")
 println()
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-    implementation("io.lettuce:lettuce-core:6.1.8.RELEASE")
-    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("io.lettuce:lettuce-core:${lettuceVersion}")
+    implementation("com.google.code.gson:gson:${gsonVersion}")
     implementation("com.formdev:flatlaf:${flatlafVersion}")
     implementation("com.formdev:flatlaf-swingx:${flatlafVersion}")
     implementation("com.formdev:flatlaf-intellij-themes:${flatlafVersion}")
     implementation("com.formdev:flatlaf-extras:${flatlafVersion}")
+    compileOnly("redis.clients:jedis:${jedisVersion}")
+    implementation("cn.hutool:hutool-extra:${hutoolVersion}")
+    implementation("org.apache.derby:derby:${derbyVersion}")
+    implementation("com.fifesoft:rsyntaxtextarea:${fifesoftVersion}")
+    implementation("com.fifesoft:rstaui:${fifesoftVersion}")
+    implementation("ch.qos.logback:logback-classic:${logbackVersion}")
     implementation("at.swimmesberger:swingx-core:1.6.8")
     implementation("com.jgoodies:jgoodies-forms:1.9.0")
-    implementation("redis.clients:jedis:${jedisVersion}")
-    implementation("cn.hutool:hutool-extra:${hutoolVersion}")
-    implementation("com.jcraft:jsch:0.1.55")
-    implementation("org.apache.derby:derby:${derbyVersion}")
     implementation("commons-net:commons-net:3.8.0")
     implementation("org.bouncycastle:bcpkix-jdk15on:1.70")
     implementation("org.bouncycastle:bcprov-jdk15on:1.70")
-    implementation("com.fifesoft:rsyntaxtextarea:${fifesoftVersion}")
-    implementation("com.fifesoft:rstaui:${fifesoftVersion}")
     implementation("com.intellij:forms_rt:7.0.3")
-    implementation("ch.qos.logback:logback-classic:1.2.11")
+    implementation("com.jcraft:jsch:0.1.55")
 }
 
+tasks.test {
+    useJUnitPlatform()
+}
 
-tasks {
+tasks.compileJava {
+     sourceCompatibility = "17"
+    targetCompatibility = "17"
+    options.encoding = "utf-8"
+    options.isDeprecation = false
+}
 
-    getByName<Test>("test") {
-        useJUnitPlatform()
+tasks.jar {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    manifest {
+        attributes("Main-Class" to "com.redisfront.RedisFrontApplication")
+        attributes("Implementation-Vendor" to "www.redisfront.com")
+        attributes("Implementation-Copyright" to "redisfront")
+        attributes("Implementation-Version" to project.version)
+        attributes("Multi-Release" to "true")
     }
 
-    withType<JavaCompile>().configureEach {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
-        options.encoding = "utf-8"
-        options.isDeprecation = false
-    }
+    exclude("module-info.class")
+    exclude("META-INF/versions/*/module-info.class")
+    exclude("META-INF/*.SF")
+    exclude("META-INF/*.DSA")
+    exclude("META-INF/*.LIST")
 
-    withType<Jar>().configureEach {
-
-        this.duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-
-        manifest {
-            attributes("Main-Class" to "com.redisfront.RedisFrontApplication")
-            attributes("Implementation-Vendor" to "www.redisfront.com")
-            attributes("Implementation-Copyright" to "redisfront")
-            attributes("Implementation-Version" to project.version)
-            attributes("Multi-Release" to "true")
-        }
-
-        exclude("module-info.class")
-        exclude("META-INF/versions/*/module-info.class")
-        exclude("META-INF/*.SF")
-        exclude("META-INF/*.DSA")
-        exclude("META-INF/*.LIST")
-
-        from({
-            configurations.runtimeClasspath.get()
-                .filter { it.name.endsWith("jar") }
-                .map {
-                    zipTree(it).matching {
-                        exclude("META-INF/LICENSE")
-                    }
+    from({
+        configurations.runtimeClasspath.get()
+            .filter { it.name.endsWith("jar") }
+            .map {
+                zipTree(it).matching {
+                    exclude("META-INF/LICENSE")
                 }
-        })
+            }
+    })
 
-        from("${rootDir}/LICENSE") {
-            into("META-INF")
-        }
-    }
-
-    withType<Javadoc>().configureEach {
-        options {
-            this as StandardJavadocDocletOptions
-            title = "${project.name} $version"
-            header = title
-            isUse = true
-            tags = listOf("uiDefault", "clientProperty")
-            addStringOption("Xdoclint:all,-missing", "-Xdoclint:all,-missing")
-            links("https://docs.oracle.com/en/java/javase/11/docs/api/")
-        }
-        isFailOnError = false
+    from("${rootDir}/LICENSE") {
+        into("META-INF")
     }
 }
+
