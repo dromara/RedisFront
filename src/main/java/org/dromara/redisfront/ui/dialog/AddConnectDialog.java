69728e797e87fff2d982c0846742b711e35ccb37
x <jpjoo@vip.qq.com>
2025-02-21 18:00:52 +0800

    fix

--- src/main/java/org/dromara/redisfront/ui/dialog/AddConnectDialog.java
+++ src/main/java/org/dromara/redisfront/ui/dialog/AddConnectDialog.java
@@ -597,7 +597,7 @@ public class AddConnectDialog extends QSDialog<RedisFrontWidget> {
         redisPanel.setLayout(new GridLayoutManager(9, 2, new Insets(0, 0, 0, 0), -1, -1));
         panel3.add(redisPanel, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         keySeparatorField = new JTextField();
-        keySeparatorField.setText(".");
+        keySeparatorField.setText(":");
         redisPanel.add(keySeparatorField, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
         sshTimeoutTextField = new JTextField();
         sshTimeoutTextField.setText("1000");
