cdc8af706b42ded1977c6cc8f6e86d04847e4cb6
xiongjianbo <jianbo.xiong@target-energysolutions.com>
2025-03-14 17:53:05 +0800

    added  "java.util.Objects" dependency

--- src/main/java/org/dromara/redisfront/ui/widget/main/fragment/scaffold/terminal/RedisFrontTerminal.java
+++ src/main/java/org/dromara/redisfront/ui/widget/main/fragment/scaffold/terminal/RedisFrontTerminal.java
@@ -15,7 +15,11 @@ import org.dromara.redisfront.ui.components.terminal.AbstractTerminal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 
 public class RedisFrontTerminal extends AbstractTerminal {
     private static final Logger log = LoggerFactory.getLogger(RedisFrontTerminal.class);
